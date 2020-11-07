4f85b28be948df8ff7b9b04d2577fe892c6aadb2
dagda25 <dagda25@gmail.com>
2020-11-07 09:14:56 +0300

    переделал offersList в функцию

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -2,36 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import OfferCard from "../offer-card/offer-card";
 
-class OffersList extends React.Component {
-  constructor(props) {
-    super(props);
+const OffersList = (props) => {
+  const {offers} = props;
 
-    this.state = {
-      activeCard: null
-    };
-  }
-
-  render() {
-    const {offers} = this.props;
-
-    const isActive = (id) => {
-      return id === this.state.activeCard;
-    };
-
-
-    return (
-      <div className="cities__places-list places__list tabs__content">
-        {offers.map((offer) => (
-          <OfferCard offer={offer} active={isActive(offer.id)} key={offer.id}/>
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offers.map((offer) => (
+        <OfferCard offer={offer} key={offer.id}/>
+      ))}
+    </div>
+  );
+};
 
 OffersList.propTypes = {
   offers: PropTypes.array.isRequired,
-  currentSorting: PropTypes.string.isRequired,
 };
 
 export default OffersList;
